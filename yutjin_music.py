6d549c234d13227166ae5007c3f9b8aa05781e24
Eugene Kim <uudoin@gmail.com>
2020-01-08 13:32:07 -0700

    email?

--- yutjin_music.py
+++ yutjin_music.py
@@ -4,7 +4,7 @@
 본 app 장점:
     Youtube보다 offline이 좋은점: internet안될때도되고, 일단 곡별로 가중치를 줄수있음
 Next:
-    git연동완료(집에도?)
+    git연동완료,email?
     scroll이 불편해: print time 보강작업:프로그램 재실행시도 보기 가능하니, 일단 메모리후 file화로
         1 print time을 강화?=>
         2 또는 체크타임 결과를 어디에 저장?
