17b0b5cc8dfde89c2a808dd9caa79e3584e53f3d
office <uudoin@gmail.com>
2020-02-03 15:08:58 -0700

    yt: playlist time

--- yutjin_yt/yutjin_yutu.py
+++ yutjin_yt/yutjin_yutu.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 # python 3X
 '''
@@ -30,11 +31,76 @@ if os.path.isfile(db_xls) == True:
         data.append(record)
 else:
     print(db_xls + " 없음")
+    
+    
+
+
 
 
 
 import urllib.request
 import json
+
+
+base_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=??????????&key=" + key
+nextPageToken = ""
+
+
+total_n = 1
+import datetime
+datetime_total = datetime.datetime(1,1,1,0,0,0)
+for n in range(2):
+    this_url = base_url + "&pageToken=" + nextPageToken
+    json_data = urllib.request.urlopen(this_url).read()     
+
+    item_data = json.loads(json_data)["items"]
+    print(len(item_data))
+    for i in range(len(item_data)):
+        item_data = json.loads(json_data)["items"][i]['snippet']['title']
+        print(str(total_n) + ":"+ item_data)        
+        videoId = json.loads(json_data)["items"][i]['snippet']['resourceId']['videoId']
+        print(videoId)
+                 
+        v_json_data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/videos?id=" + str(videoId) + "&part=contentDetails&key=" + key).read()
+        v_item_data = json.loads(v_json_data)["items"][0]["contentDetails"]['duration']
+        #debug print(v_item_data)
+        v_item_data = v_item_data.replace('PT','')        
+        m = s = 0
+        if v_item_data.find('M') == -1:
+            v_item_data = '0:' + v_item_data
+        else:
+            v_item_data = v_item_data.replace('M',':')        
+            
+        if v_item_data.find('S') == -1:
+            v_item_data = v_item_data + '0'
+        else:
+            v_item_data = v_item_data.replace('S','')        
+        
+        #print(v_item_data)        
+        date_time_obj = datetime.datetime.strptime(v_item_data, '%M:%S')        
+        datetime_now = datetime.timedelta(minutes = date_time_obj.minute, seconds = date_time_obj.second)
+        
+        datetime_total += datetime_now
+        print(datetime_now)
+        
+        print()
+        total_n += 1
+        
+        
+    if "nextPageToken" in json.loads(json_data):
+        nextPageToken = json.loads(json_data)["nextPageToken"]
+        print(nextPageToken)
+        n += 1
+    else:
+        break
+print(datetime_total)
+exit(0)
+
+
+
+
+
+
 
 for i in range(0,len(data)):
     value = data[i][1] 
