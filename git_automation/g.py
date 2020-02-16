78b596c56106016aad635e44e91084d5f6bcbb78
Home <uudoin@gmail.com>
2020-02-16 14:10:38 -0700

    v9

--- git_automation/g.py
+++ git_automation/g.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
         
     #2
     import random
-    maxx = 57
+    maxx = 58
     random_num = random.randrange(2, maxx + 1)    
     print(random_num, '/',maxx)      
     next_gap_timedelta = datetime.timedelta(minutes = random_num)
