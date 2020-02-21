ae0193d68a9982b348d16213cd84fa658f037678
Home <uudoin@gmail.com>
2020-02-21 10:37:15 -0700

    1 Round 0min

--- git_automation/g.py
+++ git_automation/g.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
         import time
         #random_num = 0  #debug
         summ = 0 
-        unit = 2
+        unit = 3
         total = 60*random_num
         while summ <= total:
             text = str(datetime.timedelta(seconds=(total - summ)))
