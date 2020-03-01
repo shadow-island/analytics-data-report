a8b30ba49386fe97a84e11f80cbb970b69ca79e0
Home <uudoin@gmail.com>
2020-03-01 05:23:29 -0700

    1 Round 0min

--- git_automation/g.py
+++ git_automation/g.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         #~
         exit(0)
         
-    work = 165
+    work = 166
     work = round(work/60 * 1.1, 1)
     #source file 이름찾기?
     import codecs
@@ -146,7 +146,7 @@ if __name__ == '__main__':
                 
         #0
         import random
-        maxx = 2*60 + 59
+        maxx = 3*60 + 0
         random_num = random.randrange(2, maxx + 1)    
         print(random_num, '/',maxx)      
         next_gap_timedelta = datetime.timedelta(minutes = random_num)
