8c922b753f7dea30ecc3e6d390e89d79d5e71134
Home <uudoin@gmail.com>
2020-03-08 00:42:35 -0700

    v0 Round 1min

--- git_automation/g.py
+++ git_automation/g.py
@@ -91,7 +91,7 @@ def timer_start():
     #print('목표' + print_time(datetime_target) + " 현재시간:" + print_time(datetime_now))    
     text = print_time(str(datetime_target - datetime_now))
     print(text + ' ', end='' , flush = True)
-    timer = threading.Timer(6,timer_start)
+    timer = threading.Timer(7,timer_start)
     #if count > 0:
     #  	date1 is considered less than date2 when date1 precedes date2 in time. (4)
     if datetime_now < datetime_target:
