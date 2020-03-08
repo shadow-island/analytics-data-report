11bcaddfe3cf2ad9a22af645ce43440729a56c80
Home <uudoin@gmail.com>
2020-03-08 04:35:32 -0600

    v0 Round 1min

--- git_automation/g.py
+++ git_automation/g.py
@@ -115,7 +115,7 @@ def run():
 
     
     import random
-    maxx = 3*60 + 19
+    maxx = 3*60 + 20
     random_num = random.randrange(2, maxx + 1)    
     #random_num = 1
     print(random_num, '/',maxx)      
@@ -162,7 +162,7 @@ if __name__ == '__main__':
         #~
         exit(0)
         
-    work = 197
+    work = 198
     work = round(work/60 * 1.1, 1)
     #source file 이름찾기?
     import codecs
