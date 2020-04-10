698f90fc25d8f4ccc85ecce8ea98cb4bf474c724
office <uudoin@gmail.com>
2020-04-09 18:50:39 -0600

    v0 Round 1min

--- git_automation/g.py
+++ git_automation/g.py
@@ -147,8 +147,8 @@ def run():
 main
 '''
 if __name__ == '__main__':
-    work    = 261
-    g_maxx  = 4*60 + 21 #(chrome RDP에 맞춤)#font26이 최대 
+    work    = 262
+    g_maxx  = 4*60 + 22 #(chrome RDP에 맞춤)#font26이 최대 
     
     import sys
     if len(sys.argv) == 1:        
