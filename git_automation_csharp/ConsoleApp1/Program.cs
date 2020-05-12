fc32057ba6b708f59e709e91ab2dc63b74e4e872
Home <uudoin@gmail.com>
2020-05-12 00:24:07 -0600

    git cs

---
+++ git_automation_csharp/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Timers;
+
+namespace gitA
+{
+    class Program
+    {
+        static int c = 0;
+        static void Main(string[] args)
+        {
+            // 읽어올 text file 의 경로를 지정 합니다.
+            string path = "eukm.log";
+            // text file 의 전체 text 를 읽어 옵니다.
+            string textValue = System.IO.File.ReadAllText(path);            
+            int numVal = Int32.Parse(textValue);
+            // 읽어온 내용을 화면에 출력 합니다.
+            Console.WriteLine("{0}", numVal);
+
+            numVal++;
+            
+            // Text 파일 생성 및 text 를 입력 합니다.
+            textValue = Convert.ToString(numVal);
+            System.IO.File.WriteAllText(path, textValue, Encoding.Default);
+            Console.WriteLine(textValue);
+
+            //--
+            System.Diagnostics.ProcessStartInfo proInfo = new System.Diagnostics.ProcessStartInfo();
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+            // 실행할 파일명 입력 -- cmd
+            proInfo.FileName = @"cmd";
+            // cmd 창 띄우기 -- true(띄우지 않기.) false(띄우기)
+            proInfo.CreateNoWindow = true;
+            proInfo.UseShellExecute = false;
+            // cmd 데이터 받기
+            proInfo.RedirectStandardOutput = true;
+            // cmd 데이터 보내기
+            proInfo.RedirectStandardInput = true;
+            // cmd 오류내용 받기
+            proInfo.RedirectStandardError = true;
+            pro.StartInfo = proInfo;
+            pro.Start();
+            // CMD 에 보낼 명령어를 입력 합니다.
+            pro.StandardInput.Write(@"git status" + Environment.NewLine);
+            pro.StandardInput.Close();
+            // 결과 값을 리턴 받습니다.
+            string resultValue = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();
+            pro.Close();
+            // 결과 값을 확인 합니다.
+            Console.WriteLine(resultValue);
+
+            Console.ReadLine();
+
+
+            // 타이머 생성 및 시작
+            Timer timer = new System.Timers.Timer();
+            timer.Interval = 15; // 단위 milisec
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Start();
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+
+        // 작업쓰레드가 지정된 시간 간격으로 아래 이벤트 핸들러 실행
+        static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {            
+            Console.WriteLine("{0}", c++);
+        }
+    }
+}
