6414a44d0b41b3a1b230b884ec88e9da2456d0d2
Home <uudoin@gmail.com>
2020-05-13 22:30:55 -0600

    csharp_v0

--- git_automation_csharp/ConsoleApp1/Program.cs
+++ git_automation_csharp/ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace gitA
 
         static void Main()
         {
-            Console.WriteLine("작업시간 {0}", 299);
+            Console.WriteLine("작업시간 {0}", 300);
             RunGit();
             Console.WriteLine("Press Enter to exit");
                         
@@ -40,7 +40,8 @@ namespace gitA
             round++;
 
             Random r = new Random();
-            int randomResult = r.Next(1, 4*60 + 50 + 1);  //
+            //int randomResult = r.Next(1, 4*60 + 50 + 1);  //
+            int randomResult = r.Next(1, 3 + 1);  //
             Console.WriteLine("randomResult {0}", randomResult);
 
             // 타이머 생성 및 시작
