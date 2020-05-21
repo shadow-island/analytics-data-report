81e9d1cd1288f34c7e955634ced8cff5d7a8bb8b
Home <uudoin@gmail.com>
2020-05-20 20:37:23 -0600

    cShar_v0_r1_20:37:23

--- git_automation_csharp/ConsoleApp1/Program.cs
+++ git_automation_csharp/ConsoleApp1/Program.cs
@@ -27,7 +27,7 @@ namespace gitA
     {
         // 읽어올 text file 의 경로를 지정 합니다.
         static readonly int roundMax    = 19;
-        static readonly int work        = 329;
+        static readonly int work        = 330;
         static readonly int tick        = 11;
         static readonly int RANDOM_MAX  = 4 * 60 + 53 + 1;//real mode
 
