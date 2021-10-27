393bbf6e10c1599b910fa03c5e9bacc26bb1181f
office <uudoin@gmail.com>
2021-10-27 16:08:02 -0600

    0 Gabon 0.0

--- git_automation_csharp/ConsoleApp/Program.cs
+++ git_automation_csharp/ConsoleApp/Program.cs
@@ -59,7 +59,7 @@ namespace gitA
     class Program
     {
         //일반개발은 2일걸렸다치고,더이상은 유지보수이므로 큰 의미없음, 이것의 목적은 대략 개발기간추정용으므로
-        static readonly float    WORK = 2.0f; //앞자리는 major 웬만하면 뒷자리로..
+        static readonly string  WORK = "2.0"; //앞자리는 major 웬만하면 뒷자리로..
         static readonly bool    debuggingMode = false;             // true false if real mode    
         // 읽어올 text file 의 경로를 지정 합니다
         static readonly string  fileGit        = "eukm.log";                
@@ -68,9 +68,9 @@ namespace gitA
 
         //목표 일일 commit개수 줄여보기 -> 같으면 성공,  실패 및 한화면안차면 10++
         //무조건  늘리지말고 일일 commit개수에 따라 늘림?
-        static int  ONE_ROUND_SIZE   = 15 * 60 + 0; //평소에는 +10, 안늘어나면 20분단위로--1년안에는 결과 나오게, 
+        static int  ONE_ROUND_SIZE   = 15 * 60 + 10; //평소에는 +10, 안늘어나면 20분단위로--1년안에는 결과 나오게, 
         static int  NEED_UPDATE_MAX  = 16;
-        static int  RANDOM_STOP_MAX  = 32;
+        static int  RANDOM_STOP_MAX  = 33;
 
         // global
         static int  _round = 0;
@@ -169,13 +169,14 @@ namespace gitA
             makeTexts(ref sNeedUpdate, ref sLocation, ref sMingling, ref sIchiMae);        
 
             //4.국가 만들기
-            string sCapital;
+            string sQuiz;
             string[] capitalList = new string[] {
                 "Benin","Porto-Novo",
                 "Botswana","Gaborone",
                 "Burundi","Bujumbura",
                 "Gabon","Libreville",
                 "Guinea","Conakry",
+                "Marshall Islands","Majuro",
                 "Mauritius","Port Louis",
                 "Mozambique","Maputo",
                 "Niger","Niamey",
@@ -193,9 +194,9 @@ namespace gitA
             
             //답지 때문에 변수 r 필요함 
             r = random.Next(0, capitalList.Length);
-            sCapital = capitalList[r];
+            sQuiz = capitalList[r];
             //~
-            sCapital = sCapital + " ";
+            sQuiz = sQuiz + " ";
 
             string sAnswer;
             if (r % 2 == 0)
@@ -267,7 +268,7 @@ namespace gitA
             //~
 
             RunCommand("git commit --all -m "
-                + "\"" + sNeedUpdate + sLocation + sMingling + sIchiMae + sCapital + sTargetHour4Commit + "\"");
+                + "\"" + sNeedUpdate + sLocation + sMingling + sIchiMae + sQuiz + sTargetHour4Commit + "\"");
 
             //home mode아닐때만
             Console.WriteLine("sLocation={0}", sLocation);
@@ -352,7 +353,7 @@ namespace gitA
             //3.조사 만들기-절반은 패스(공백)            
             if (0 == random.Next(0, 2))
             {
-                string[] cong = new string[] { "by", "from", "in", "of" };
+                string[] cong = new string[] { "by", "from", "in", "of", "/" };
                 sIchiMae = RandomString(cong);
                 sIchiMae += " ";
             }
